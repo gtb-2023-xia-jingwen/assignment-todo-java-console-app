777ce2a5f77ca8e06cd62135e6f5e05fe20864cc
Jingwen Xia <amyfor7@gmail.com>
2022-10-21 10:55:14 +0800

    [Jingwen Xia] chore: delete the greeting messgage

--- src/main/java/tw/cn/cap/gtb/todo/App.java
+++ src/main/java/tw/cn/cap/gtb/todo/App.java
@@ -1,11 +1,8 @@
 package tw.cn.cap.gtb.todo;
 
 public class App {
-    public String getGreeting() {
-        return "Hello World!";
-    }
 
     public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
+
     }
 }
