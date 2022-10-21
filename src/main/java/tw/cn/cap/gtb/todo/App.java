0677741c35b19c52911b6268a04d653a0c9cb5a2
Jingwen Xia <amyfor7@gmail.com>
2022-10-21 12:13:49 +0800

    [Jingwen Xia] feat: use init command to do some initial operations

--- src/main/java/tw/cn/cap/gtb/todo/App.java
+++ src/main/java/tw/cn/cap/gtb/todo/App.java
@@ -1,8 +1,27 @@
 package tw.cn.cap.gtb.todo;
 
+import java.io.File;
+import java.io.IOException;
+import java.lang.reflect.Field;
+
 public class App {
+    static final String HOME = System.getProperty("user.home");
+    static final String TASK_DIR = HOME + "/.todo/";
+    static final String TASK_FILE = TASK_DIR + "tasks";
 
-    public static void main(String[] args) {
+    public void init() throws IOException {
+        File taskDir = new File(TASK_DIR);
+        if (!taskDir.exists()) taskDir.mkdirs();
+        File taskFile = new File(TASK_FILE);
+        if (!taskFile.exists()) taskFile.createNewFile();
+        System.out.println("Initialized successfully.");
+    }
 
+    public static void main(String[] args) throws IOException {
+        App app = new App();
+        String cmd = args[0];
+        if (cmd.equals("init")) {
+            app.init();
+        }
     }
 }
